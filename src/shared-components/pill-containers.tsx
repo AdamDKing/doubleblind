4499ddc270173f08e9cb8cc77aca3ccbbe36b0ca
Adam King <adamdentonking@gmail.com>
2021-09-10 11:17:28 -0400

    make pillcell and pillcontainers align with eslint

--- src/shared-components/pill-containers.tsx
+++ src/shared-components/pill-containers.tsx
@@ -1,8 +1,5 @@
-import React, {createRef, useEffect, useRef, useState} from 'react';
-import {serialize} from 'v8';
-import {PILL} from '../enums';
+import React, {useEffect, useRef} from 'react';
 import {PillCell} from './pillcell';
-import Xarrow from 'react-xarrows';
 
 interface IPillContainersProps {
   size: number,
@@ -14,67 +11,95 @@ interface IPillContainersProps {
 
 /**
  * A full pillcontainers diagram
- * @param props
- * @returns 
+ * contains divs for "pillweek"s, which are containers
+ * and those contain pillcells.
+ * @param props size, containersize, arrow and highlighting
+ * @returns TSX
  */
-export function PillContainers(props: IPillContainersProps) {
+export function PillContainers(props: IPillContainersProps): JSX.Element {
   const getCellColor = (i: number) => {
     if (i >= props.size) return 'darkgray';
     if (props.highlight && props.highlight.has(i)) return 'yellow';
     return '';
-  } 
+  };
   const numContainers = Math.ceil(props.size/props.containerSize);
   const pillCells = Array(numContainers*props.containerSize).fill(0)
-    .map((r, i) => <PillCell number={i} numVis={i < props.size} key={i} color={getCellColor(i)}/>)
+      .map((r, i) =>
+        <PillCell number={i} numVis={i < props.size} key={i} color={getCellColor(i)}/>,
+      );
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const cWidth = props.containerSize*64 + 14;
   const cHeight = numContainers*81 + 5;
   const getCoords = (cellNum: number): [number, number] => {
-    return [64*(cellNum % props.containerSize)+39, 86*Math.floor(cellNum / props.containerSize)+43]
-  }
+    return [64*(cellNum % props.containerSize)+39, 86*Math.floor(cellNum / props.containerSize)+43];
+  };
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas?.getContext('2d');
     if (canvas && context && props.arrow) {
       ((ctx) => {
-      ctx.clearRect(0,0, canvas.width, canvas.height)
-      ctx.beginPath()
-      canvas_arrow(ctx, ...getCoords(props.arrow[0]), ...getCoords(props.arrow[1]))
-      ctx.stroke()})(context)
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        canvasArrow(ctx, ...getCoords(props.arrow[0]), ...getCoords(props.arrow[1]));
+        ctx.stroke()
+        ;
+      })(context);
     }
-  }, [props.arrow])
+  }, [props.arrow]);
   return (
     <>
-    <div className="pillcontainers">
-      <div className="pillweeks">
-      {
-        Array(numContainers).fill(0).map((v, container) => {
-          return (
-            <div className="pillweek" style={{width: `${props.containerSize*64}px`}} key={container}>{
-              pillCells.slice(container*props.containerSize, (container+1)*props.containerSize)
-            }</div>
-          );
-        })
-      }
-      </div>
-      <canvas ref={canvasRef} width={cWidth} height={cHeight} />
+      <div className="pillcontainers">
+        <div className="pillweeks">
+          {
+            Array(numContainers).fill(0).map((v, container) => {
+              return (
+                <div
+                  className="pillweek"
+                  style={{width: `${props.containerSize*64}px`}}
+                  key={container}
+                >
+                  {pillCells.slice(container*props.containerSize,
+                      (container+1)*props.containerSize)}
+                </div>
+              );
+            })
+          }
+        </div>
+        <canvas ref={canvasRef} width={cWidth} height={cHeight} />
       </div>
     </>
   );
 }
-//Yoinked from https://stackoverflow.com/questions/808826/draw-arrow-on-canvas-tag
-function canvas_arrow(context:CanvasRenderingContext2D, fromx:number, fromy:number, tox:number, toy:number) {
-  var headlen = 10; // length of head in pixels
-  var dx = tox - fromx;
-  var dy = toy - fromy;
-  var angle = Math.atan2(dy, dx);
+
+/** Draws a double-headed arrow between two points on a canvas
+ *
+ * This function is modified from one on SO:
+ * https://stackoverflow.com/questions/808826/draw-arrow-on-canvas-tag
+ *
+ * @param context canvas context to draw arrow on
+ * @param fromx x coord 1
+ * @param fromy y coord 1
+ * @param tox x coord 2
+ * @param toy y coord 2
+ * @returns void
+ */
+function canvasArrow(
+    context:CanvasRenderingContext2D, fromx:number, fromy:number, tox:number, toy:number): void {
+  const headlen = 10; // length of head in pixels
+  const dx = tox - fromx;
+  const dy = toy - fromy;
+  const angle = Math.atan2(dy, dx);
   context.moveTo(fromx, fromy);
-  context.lineTo(fromx + headlen * Math.cos(angle - Math.PI / 6), fromy + headlen * Math.sin(angle - Math.PI / 6));
+  context.lineTo(fromx + headlen * Math.cos(angle - Math.PI / 6),
+      fromy + headlen * Math.sin(angle - Math.PI / 6));
   context.moveTo(fromx, fromy);
-  context.lineTo(fromx + headlen * Math.cos(angle + Math.PI / 6), fromy + headlen * Math.sin(angle + Math.PI / 6));
+  context.lineTo(fromx + headlen * Math.cos(angle + Math.PI / 6),
+      fromy + headlen * Math.sin(angle + Math.PI / 6));
   context.moveTo(fromx, fromy);
   context.lineTo(tox, toy);
-  context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
+  context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6),
+      toy - headlen * Math.sin(angle - Math.PI / 6));
   context.moveTo(tox, toy);
-  context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+  context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6),
+      toy - headlen * Math.sin(angle + Math.PI / 6));
 }
--- src/shared-components/pillcell.tsx
+++ src/shared-components/pillcell.tsx
@@ -1,22 +1,29 @@
-import React, {RefObject, useState} from 'react';
-import {MutableRefObject} from 'react';
-import {preProcessFile} from 'typescript';
-
-/**
- * A single cell in a diagram of pill containers.
- */
-export function PillCell(props: PillCellProps) {
-  return (
-    <div className="pillcell" style={{backgroundColor: props.color ? props.color : '#dfdfdf'}} key={props.number}>
-      {props.numVis ? <div className="cellnumber">{props.number+1}</div> : <div className="cellnumber">-</div>}
-      {props.contents}
-    </div>
-  );
-}
+import React from 'react';
 
 interface PillCellProps {
   number: number,
   contents?: any,
   color?: string,
   numVis: boolean,
+}
+
+/**
+ * A single cell in a diagram of pill containers.
+ *
+ * @param props number, contents, whether the number is visible, and color
+ * @returns TSX for a single pillcell
+ */
+export function PillCell(props: PillCellProps) {
+  return (
+    <div
+      className="pillcell"
+      style={{backgroundColor: props.color ? props.color : '#dfdfdf'}}
+      key={props.number}
+    >
+      <div className="cellnumber">
+        {props.numVis ? props.number+1 : '-'}
+      </div>
+      {props.contents}
+    </div>
+  );
 }
