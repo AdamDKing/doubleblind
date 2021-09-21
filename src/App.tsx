bb705059385183af3d9a5f6e56e0897490d77fde
Adam King <adamdentonking@gmail.com>
2021-09-21 18:05:33 -0400

    remove old comment from App about flow phases

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,6 @@ import {Header} from './layout-components/header';
  * @returns React App
  */
 function App() {
-  // lets put some functionality here before splitting it out
-  // into a middle layer
-  // maybe higher order?
-  // I want (1) testable function components, determined by props
-  // and (2) simple, straightforward, back + forward functionality
-  // I *dont* want a bunch of useful mutable state packaged up for the entire
-  // flow. Distinct steps (function calls), immutable data.
-  // Some phases have mutable data internally though (the formik)
-
-  // This whole approach may have been a mistake.  I think I'll leave it
-  // and make progress elsewhere.  Will return when it causes bugs.
-
-
   // const leavePageWarning = (e: BeforeUnloadEvent) => {
   //   e.preventDefault();
   //   e.returnValue = 'Are you sure you want to leave? All progress will be lost.';
@@ -51,4 +38,3 @@ interface PhaseContext {
   back: () => void;
 }
 export const phaseContext = React.createContext<PhaseContext>({next: (c) => {}, back: () => {}});
-
