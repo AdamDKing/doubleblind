d892d421206181b999afa7a323692bb5d1719e56
Adam King <adamdentonking@gmail.com>
2021-09-24 09:54:54 -0400

    add router and start on about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable max-len */
 import React from 'react';
+import {Route, Switch} from 'react-router';
+import {BrowserRouter} from 'react-router-dom';
 import {DblndFlow} from './layout-components/dblnd-flow';
 import {Header} from './layout-components/header';
 
@@ -25,14 +29,39 @@ function App() {
 
   return (
     <>
-      <Header />
-      <DblndFlow />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <DblndFlow />
+          </Route>
+          <Route path="/about">
+            <div className="textpage">
+              <section>
+                <h1>About this site</h1>
+                <p>
+                Doubleblind.me makes it easy to set up and run double-blinded experiments.
+                 It exists because the author (Adam) wanted a similar tool but couldn't find one online.
+                 Double-blinding an experiment removes the <a href="https://en.wikipedia.org/wiki/Placebo">placebo effect</a>
+                 from the result.  Anyone interested in substances that may improve their health
+                 and well-being should improve their experiments by double-blinding them.
+                </p>
+              </section>
+              <p>
+                This site only helps with double-blinding, it doesn't help you with assessments or offline setup, other than providing a guide.
+
+              </p>
+            </div>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
 
 export default App;
 
+// possibly should live elsewhere
 interface PhaseContext {
   next: (component: JSX.Element) => void;
   back: () => void;
