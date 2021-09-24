429e7d5b2aad4231f9a6dd3161cf02feffa44b94
Adam King <adamdentonking@gmail.com>
2021-09-24 16:59:27 -0400

    add keyfile img and short guide to guide

--- src/guide/guide.tsx
+++ src/guide/guide.tsx
@@ -5,6 +5,8 @@ import capsules from './capsules-cropped.jpg';
 import pillcont from './pillcont-cropped.jpg';
 import pillcontNumbered from './pillcont-numbered-cropped.jpg';
 import pillcontFilled from './pillcont-numbered-filled-cropped.jpg';
+import keyfile from './keyfile.jpg';
+import {NavLink} from 'react-router-dom';
 
 interface GuideProps {
 }
@@ -16,7 +18,25 @@ interface GuideProps {
  */
 export function Guide(props: GuideProps) {
   return <div className="textpage">
-    <h1>Starting an Experiment</h1>
+    <h1>
+      Short Guide
+    </h1>
+    <p>
+      You will need to fill two sets of indistinguishable capsules:
+        one set with the substance you are testing
+        (the <em>treatment</em>), the other with some inert
+        substance like sugar (the <em>control</em>, a placebo).
+        This site will then walk you and a helper through the process
+        of filling up a weekly/monthly pill organizer with your capsules,
+        ordered randomly so that neither you nor your helper know
+        which days' capsules contain which substances.
+        To accomplish this, you and your helper will do different parts
+        of the randomization without communcating.
+        At the end of this process, you'll download a key
+        that will let you know the real contents for each capsule --
+        make sure not to check it until your experiment is done.
+    </p>
+    <h1>Long Guide (in progress!)</h1>
     <p>
     To get started you need something to test, opaque capsules, and pill containers.
      For this guide we'll proceed as if we're testing L-Theanine taken with coffee each morning.
@@ -38,12 +58,13 @@ export function Guide(props: GuideProps) {
     </p>
     <div className="imageset"></div>
     <p>
-    At this point you have all the raw materials for the doubleblinding process.  Enlist a helper and click back to <a href="#">home</a> to get started.
+    At this point you have all the raw materials for the doubleblinding process.  Enlist a helper and click back to <NavLink to="/">home</NavLink> to get started.
     Once you're done, you'll have your control and treatment pills randomly placed in your pill containers, and you'll have a key file that reveals the real contents
     of each capsule, to consult after your experiment is over.
     </p>
     <div className="imageset">
       <img src={pillcontFilled}/>
+      <img src={keyfile}/>
     </div>
   </div>;
 }
