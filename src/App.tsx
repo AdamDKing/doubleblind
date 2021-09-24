ecfce7dc264c3a4b417aa085679286eeb7002569
Adam King <adamdentonking@gmail.com>
2021-09-24 10:59:35 -0400

    about page version 1

--- src/App.tsx
+++ src/App.tsx
@@ -37,19 +37,37 @@ function App() {
           </Route>
           <Route path="/about">
             <div className="textpage">
-              <section>
-                <h1>About this site</h1>
-                <p>
-                Doubleblind.me makes it easy to set up and run double-blinded experiments.
-                 It exists because the author (Adam) wanted a similar tool but couldn't find one online.
-                 Double-blinding an experiment removes the <a href="https://en.wikipedia.org/wiki/Placebo">placebo effect</a>
-                 from the result.  Anyone interested in substances that may improve their health
-                 and well-being should improve their experiments by double-blinding them.
-                </p>
-              </section>
+              <h1>About this site</h1>
               <p>
-                This site only helps with double-blinding, it doesn't help you with assessments or offline setup, other than providing a guide.
-
+                Doubleblind.me makes it easy to set up and run double-blinded experiments.
+                 It exists because its author wanted a similar tool but couldn't find one online (and because the author is a fan of citizen science).
+                 Double-blinding an experiment accounts for the <a href="https://en.wikipedia.org/wiki/Placebo">placebo effect</a> in
+                 the result.  Anyone experimenting with substances that may improve their health
+                 and well-being should improve their experiments by double-blinding them.
+              </p>
+              <p>
+                 The kind of experiments you can double blind involve substances in opaque capsules and
+                 assessments of the efficacy of those substances.  This site helps with double-blinding;
+                 it doesn't help you with assessments.  Some examples of substances and assessments:
+                <ul>
+                  <li>
+                     Melatonin -&gt; sleep hours and subjective wakefulness
+                  </li>
+                  <li>
+                     L-Theanine (taken with caffiene) -&gt; subjective "jitteriness"
+                  </li>
+                </ul>
+                Ultimately the goal of an experiment is to discover something that works for you, so the assessment should
+                 be one that you're comfortable with.  This author is inclined towards written subjective assessments and a number rating, but you should feel free to use more rigorous methods.
+              </p>
+            </div>
+          </Route>
+          <Route path="/guide">
+            <div className="textpage">
+              <h1>Starting an Experiment</h1>
+              <p>
+                To get started, you need something to test, capsules, and pill containers.
+                 For this guide we'll proceed as if we're testing L-Theanine taken with coffee each morning.
               </p>
             </div>
           </Route>
